28f054e36f30fc8b791ba38a5b41589a2bedd812
Mike <21209421+mtzfox@users.noreply.github.com>
2022-12-20 17:44:50 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ const app = createApp(App);
 
 app.use(
   createAuth0({
-    domain: "dev-nq6cgd3w773e7fxn.us.auth0.com",
-    client_id: "ABzoL7M4BtDtDTA9R4OdLzyHtoXGA4Zl",
+    domain: ,
+    client_id: ,
     redirect_uri: window.location.origin
   })
 );
